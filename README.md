9bf2dccf2ad47e2fe46dcf4bb279194db47ebb96
varrbor <varrbor@ukr.net>
2023-09-09 19:40:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-form-challange
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-il9urv)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+export default function App() {
+  return (
+    <div>
+      <LoginForm/>
+    </div>
+  );
+}
